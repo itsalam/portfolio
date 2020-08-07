d958c0c38b5ebb2b36a1e07e414b2cb679635313
unknown <vincentthanhlam@gmail.com>
2020-08-07 13:50:24 -0700

    changed background font

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -3,11 +3,11 @@ import { PortfolioData } from '../Models/portfolioData';
 import anime from "animejs";
 import Particles from 'react-particles-js';
 import './HomePage.scss';
+import { Transition } from 'react-transition-group';
 var particleData = require("../particles.json");
 
-
-
 class HomePage extends Component<PortfolioData> {
+
    onYouTubeIframeAPIReady() {
       var player;
       player = new YT.Player('YouTubeBackgroundVideoPlayer', {
@@ -36,15 +36,20 @@ class HomePage extends Component<PortfolioData> {
                 e.target.playVideo();
             },
             onStateChange: function(e) {
+             
               if (e.data === 1) { // fade out #_buffering-background
                   anime({
-                     duration: 400,
+                     duration: 1000,
                      targets: document.getElementById('_buffering-background'),
-                     opacity: [100, 0],
-                     easing: "easeInOutQuart",
+                     opacity: [1, 0],
                   })
+                  // if (!this.state.title){
+                  //    this.showTitle();
+                  // }
               }
               if (e.data === 0) { // loop video
+                  e.target.mute();
+                  e.target.setPlaybackQuality('hd1080');
                   e.target.playVideo();
               }
             }
@@ -63,6 +68,7 @@ class HomePage extends Component<PortfolioData> {
          }).then(value => this.setState({ ...value }));
       this.onYouTubeIframeAPIReady()
    }
+
    render() {
 
       var data = this.props;
@@ -77,20 +83,26 @@ class HomePage extends Component<PortfolioData> {
       return (
          <header id="home">
 
-            
-            <div className="video-background">
-               <div id="_buffering-background"></div>
-               <div className="video-foreground" id="YouTubeBackgroundVideoPlayer">
+            <Transition
+               in={true}
+               timeout={10000}>
+               <div className="video-background">
+                  {/* <div className="pattern-overlay"></div> */}
+                  <div id="_buffering-background"></div>
+                  <div className="video-foreground" id="YouTubeBackgroundVideoPlayer">
+                  </div>
                </div>
-            </div>
+            </Transition>
             <Particles className="particles" params={particleData} />
 
             <div className="row banner">
 
                <div className="banner-text">
-
-                  <h1 className="responsive-headline">I'm {data.name}.</h1>
-                  <h3>I'm a {data.address.city} based <span>{data.occupation}</span>. {data.description}.</h3>
+                  <div className="title-text">
+                     <h1 className="responsive-headline" id="title">Hi, I'm {data.name}.</h1>
+                  </div>
+                  <h3> Hey don't look at this yet its not ready C: </h3>
+                  {/* <h3>I'm a {data.address.city} based <span>{data.occupation}</span>. {data.description}.</h3> */}
                   <hr />
                   <ul className="social">
                      {networks}
